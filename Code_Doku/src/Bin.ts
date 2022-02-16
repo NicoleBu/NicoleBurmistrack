194d3c76fa71c6568ba08c96df706d5f03abcc35
NicoleBu <43846727+NicoleBu@users.noreply.github.com>
2022-02-16 16:45:23 +0100

    Update Bin.ts

--- Code_Doku/src/Bin.ts
+++ Code_Doku/src/Bin.ts
@@ -1,4 +1,5 @@
 namespace RestaurantSimulation{
+    // Klasse Bin erbt von Klasse Entity. Entity braucht Vector um erzeugt zu werden. Super-Call führt dazu, dass bei Erzeugung von Klasse erst die Super-Klasse angesteuert wird
     export class Bin extends Entity{
         constructor( pos: Vector) {
             super(pos);
